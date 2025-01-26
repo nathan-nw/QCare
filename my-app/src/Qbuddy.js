25cb5f5435264738dbae153493baa2e6e78d33ea
Nathan <nwan23@uwo.ca>
2025-01-26 00:45:52 -0500

    Sign in

--- my-app/src/Qbuddy.js
+++ my-app/src/Qbuddy.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Qbuddy.css";
+import { FaCircleArrowUp } from "react-icons/fa6";
 
 function Qbuddy() {
   const [messages, setMessages] = useState([]);
@@ -18,28 +19,28 @@ function Qbuddy() {
     <div className="container">
       <div className="title">Your Friendly Qbuddy!</div>
 
-      <div className="card">
-        <div className="messages">
-          {messages.map((message, index) => (
-            <div key={index} className={`chatbot-message chatbot-message-user`}>
-              {message.text}
-            </div>
-          ))}
-        </div>
-
-        <div className="input-container">
-          <input
-            className="input"
-            placeholder="Type your message..."
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyPress={(e) => e.key === "Enter" && handleSendMessage()}
-          />
-          <button className="button" onClick={handleSendMessage}>
-            Send
-          </button>
-        </div>
+      {/* <div className="card"> */}
+      <div className="messages">
+        {messages.map((message, index) => (
+          <div key={index} className={`chatbot-message chatbot-message-user`}>
+            {message.text}
+          </div>
+        ))}
       </div>
+
+      <div className="input-container">
+        <input
+          className="input"
+          placeholder="Type your message..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyPress={(e) => e.key === "Enter" && handleSendMessage()}
+        />
+        <FaCircleArrowUp className="button" onClick={handleSendMessage}>
+          Send
+        </FaCircleArrowUp>
+      </div>
+      {/* </div> */}
     </div>
   );
 }
--- my-app/src/Signin.js
+++ my-app/src/Signin.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Signin.css";
 
 export default function Signin() {
   return (
-    <div>
-      <h1>Signin</h1>
+    <div className="signin-container">
+      <h1 className="signin-header">ENTER PATIENT ID</h1>
+      <input
+        type="text"
+        placeholder="Patient ID"
+        className="signin-input"
+      />
+      <button className="signin-button">Sign In</button>
     </div>
   );
 }
