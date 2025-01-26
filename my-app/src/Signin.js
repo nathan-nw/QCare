2bc79e2b78d884a5a6969f6648b0973457c50ae8
Nathan <nwan23@uwo.ca>
2025-01-26 02:34:42 -0500

    f

--- my-app/src/Signin.js
+++ my-app/src/Signin.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./Signin.css";
+import QCareLogo from "./images/QCAREFINALLOGO.png"; // Import the PNG file
 
-import logo from "./images/QCare.png";
 export default function Signin() {
   return (
     <div className="signin-container">
-      <img src={logo} className="signin-logo" alt="logo" />
-      <h1 className="signin-header">ENTER PATIENT ID</h1>
-      <input type="text" placeholder="Patient ID" className="signin-input" />
-      <button className="signin-button">Sign In</button>
+      <div className="signin-top">
+        <img src={QCareLogo} alt="QCare logo" className="signin-logo" />
+      </div>
+      <div className="signin-bottom">
+        <h1 className="signin-header">Enter Patient ID</h1>
+        <input
+          type="text"
+          placeholder="Patient ID"
+          className="signin-input"
+          aria-label="Patient ID"
+        />
+        <button className="signin-button">Sign In</button>
+      </div>
     </div>
   );
 }
