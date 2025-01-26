f1d6c64ade1ae33972816c015530b9e9b8b7ce01
Millicent-Song <msong257@uwo.ca>
2025-01-26 01:54:15 -0500

    Backend data showing

--- Backend/firebase.js
+++ Backend/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase} from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -7,6 +8,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: "AIzaSyDlNvBdpI3u-VYiMIBTuU4ao4KgSEpmELc",
   authDomain: "qcare-b7741.firebaseapp.com",
+  databaseURL: "https://console.firebase.google.com/u/0/project/qcare-b7741/database/qcare-b7741-default-rtdb/data/~2F",
   projectId: "qcare-b7741",
   storageBucket: "qcare-b7741.firebasestorage.app",
   messagingSenderId: "252454746421",
@@ -16,6 +18,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore and Auth
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const database = getDatabase(app);
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signin from "./Signin";
 import Home from "./Home";
 import Qbuddy from "./Qbuddy";
+import TestFetch from "./testFetch"; // Correctly imported
 
 export default function App() {
   return (
@@ -10,7 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/Signin" element={<Signin />} />
         <Route path="/" element={<Home />} />
-        <Route path="/Qbuddy" element={<Qbuddy/>} />
+        <Route path="/Qbuddy" element={<Qbuddy />} />
+        <Route path="/testFetch" element={<TestFetch />} /> {/* Correctly used */}
       </Routes>
     </Router>
   );
